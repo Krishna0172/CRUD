efbf1bc86776bde2201aeb6d141927c266861e94
Krishna0172 <152146847+Krishna0172@users.noreply.github.com>
2023-12-20 14:31:26 +0530

    Add files via upload

--- queries.js
+++ queries.js
@@ -1,13 +1,17 @@
-
 const getAllUsersQuery = 'SELECT * FROM users ORDER BY id ASC';
 
-const insertUserQuery = (user) => {
-  return `INSERT INTO users(id, name, age, dep) VALUES (${user.id}, '${user.name}', '${user.age}', '${user.dep}')`;
-};
+const insertUserQuery = (user) => `
+  INSERT INTO users(id, name, age, dep)
+  VALUES (${user.id}, '${user.name}', '${user.age}', '${user.dep}')
+`;
 
 const getUserByIdQuery = (id) => `SELECT * FROM users WHERE id=${id}`;
 
-const updateUserQuery = (user) => `UPDATE users SET name='${user.name}', age ='${user.age}', dep ='${user.dep}' WHERE id=${user.id}`;
+const updateUserQuery = (user) => `
+  UPDATE users
+  SET name='${user.name}', age ='${user.age}', dep ='${user.dep}'
+  WHERE id=${user.id}
+`;
 
 const deleteUserQuery = (id) => `DELETE FROM users WHERE id=${id}`;
 
